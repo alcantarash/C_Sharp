638901a9e842516dadb731948cc30ea371acc5a1
Sheldon <sheldongoulart-92@hotmail.com>
2019-09-09 17:25:09 -0300

    Post and Async

--- UnitTestProject1/UnitTestProject1/UnitTest1.cs
+++ UnitTestProject1/UnitTestProject1/UnitTest1.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NUnit.Framework;
 using RestSharp;
 using RestSharp.Deserializers;
 using RestSharp.Serialization.Json;
+using UnitTestProject1.Model;
 
 namespace UnitTestProject1
 {
@@ -27,6 +29,75 @@ namespace UnitTestProject1
             NUnit.Framework.Assert.That(result, Is.EqualTo("typicode"), "Incorrect!");
         }
 
+        [TestMethod]
+        public void PostWithAnonymousBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/{postid}/profile", Method.POST);
 
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new { name = "Rajesh" });
+
+            request.AddUrlSegment("postid", 1);
+            var response = client.Execute(request);
+
+            var deserialize = new JsonDeserializer();
+            var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            var result = output["name"];
+
+            NUnit.Framework.Assert.That(result, Is.EqualTo("Rajesh"), "Incorrect!");
+        }
+
+        [TestMethod]
+        public void PostWithTypeClassBody()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/", Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new Posts { id = "4", author = "API Test", title = "RestSharp API" });
+
+            var response = client.Execute<Posts>(request);
+
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["author"];
+
+            NUnit.Framework.Assert.That(response.Data.author, Is.EqualTo("API Test"), "Incorrect!");
+        }
+
+        [TestMethod]
+        public void PostWithAsync()
+        {
+            var client = new RestClient("http://localhost:3000/");
+            var request = new RestRequest("posts/", Method.POST);
+
+            request.RequestFormat = DataFormat.Json;
+            request.AddJsonBody(new Posts { id = "4", author = "API Test", title = "RestSharp API" });
+
+            var response = client.Execute<Posts>(request);
+
+            //var deserialize = new JsonDeserializer();
+            //var output = deserialize.Deserialize<Dictionary<string, string>>(response);
+            //var result = output["author"];
+
+            NUnit.Framework.Assert.That(response.Data.author, Is.EqualTo("API Test"), "Incorrect!");
+        }
+
+        private async Task<IRestResponse<T>> ExecuteAsyncRequest<T>(RestClient client, IRestRequest request)
+        {
+            var taskCompletionSource = new TaskCompletionSource<IRestResponse<T>>();
+
+            client.ExecuteAsync<T>(request, restresponse =>
+            {
+                if (restresponse.ErrorException != null)
+                {
+                    const string message = "Error retrieving response.";
+                    throw new ApplicationException(message, restresponse.ErrorException);
+                }
+                taskCompletionSource.SetResult(restresponse);
+            });
+            return await taskCompletionSource.Task;
+        }
     }
 }

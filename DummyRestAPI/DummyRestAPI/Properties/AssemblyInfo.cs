1f29895e508b75431ede9432f658a5f8a907b3ee
Sheldon <sheldongoulart-92@hotmail.com>
2019-09-10 16:48:12 -0300

    Dummy Example Rest API

---
+++ DummyRestAPI/DummyRestAPI/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DummyRestAPI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DummyRestAPI")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e72c7278-7798-4078-878a-b4fb5cfc5d52")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DummyRestAPI/DummyRestAPI/UnitTest1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+//using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json.Linq;
+using NUnit.Framework;
+using RestSharp;
+using RestSharp.Serialization.Json;
+
+namespace DummyRestAPI
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        public string url = "http://dummy.restapiexample.com/api/v1/";
+        [Test]
+        public void TestMethod1()
+        {
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest("employees", Method.GET);
+            IRestResponse restResponse = client.Execute(request);
+
+            string response = restResponse.Content;
+
+            if (!response.Contains("156696"))
+            {
+                Assert.Fail("Wrong ID");
+            }
+        }
+        [Test]
+        public void GetRequestbyID()
+        {
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest("employee/{id}", Method.GET);
+
+            request.AddUrlSegment("id", 156702);
+            var response = client.Execute(request);
+
+            JObject obs = JObject.Parse(response.Content);
+            Assert.That(obs["employee_name"].ToString(), Is.EqualTo("test333"), "Nome do empregado Ok!");
+        }
+        [Test]
+        public void PostRequest()
+        {
+            RestClient client = new RestClient(url);
+            RestRequest request = new RestRequest("create", Method.POST);
+
+            var response = client.Execute(request);
+
+            response.StatusCode.Equals(200);
+        }
+    }
+}

7193064c631a3193120457d6d265cb35b2703777
Sheldon <sheldongoulart-92@hotmail.com>
2019-09-11 17:48:42 -0300

    Ajusts

---
+++ DummyRestAPI/DummyRestAPI/Models/employee.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DummyRestAPI.Models
+{
+    public class Employee
+    {
+        public string id { get; set; }
+        public string name { get; set; }
+        public int salary { get; set; }
+        public int age { get; set; }
+
+        //public Employee(string Name, int Salary, int Age)
+        //{
+        //    this.Name = Name;
+        //    this.Salary = Salary;
+        //    this.Age = Age;
+        //}
+    }
+}
--- DummyRestAPI/DummyRestAPI/UnitTest1.cs
+++ DummyRestAPI/DummyRestAPI/UnitTest1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using DummyRestAPI.Libraries;
+using DummyRestAPI.Models;
 //using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json.Linq;
 using NUnit.Framework;
@@ -22,7 +23,7 @@ namespace DummyRestAPI
 
             string response = restResponse.Content;
 
-            if (!response.Contains("156696"))
+            if (!response.Contains("160705"))
             {
                 Assert.Fail("Wrong ID");
             }
@@ -43,28 +44,63 @@ namespace DummyRestAPI
         public void PostRequest()
         {
             RestClient client = new RestClient(url);
-            RestRequest request = new RestRequest("create", Method.POST);
+            RestRequest request = new RestRequest("create", Method.POST)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddJsonBody(new Employee{ name = "Who13", salary = 10000, age = 10 });
 
             var response = client.Execute(request);
+            int statusCode = (int)response.StatusCode;
 
-            response.StatusCode.Equals(200);
+            if (statusCode == 200)
+            {
+                if (response.Content.Contains("Who13"))
+                {
+                    var id_employee = response.DeserializeResponse()["id"];
+                    var name_employee = response.DeserializeResponse()["name"];
+                    Console.WriteLine("This is ID code generated: " + id_employee);
+                }
+                else
+                {
+                    Assert.Fail("Error Request! - First Else");
+                    Console.WriteLine(response.Content);
+                }
+            }
+            else
+            {
+                Assert.Fail("Error Request! - Second Else");
+                Console.WriteLine(response.Content);
+            }
         }
         [Test]
         public void PutRequest()
         {
             RestClient client = new RestClient(url);
-            RestRequest request = new RestRequest("update/{id}", Method.PUT);
-
-            request.RequestFormat = DataFormat.Json;
-            request.AddJsonBody(new { name = "Raj", salary = "1", age = "100" });
-            request.AddUrlSegment("id", 156696);
+            RestRequest request = new RestRequest("update/{id}", Method.PUT)
+            {
+                RequestFormat = DataFormat.Json
+            };
+            request.AddJsonBody(new Employee{ name = "Raj1", salary = 3, age = 100 });
+            request.AddUrlSegment("id", 161050);
 
             var response = client.Execute(request);
-            var result = response.DeserializeResponse()["name"];
 
-            if (response.StatusCode.Equals(200))
+            //Console.WriteLine(response.Content);
+
+            int statusCode = (int)response.StatusCode;
+            if (statusCode == 200)
             {
-                Assert.That(response, Is.EqualTo("Raj"), "Editing Done!");
+                if (response.Content.Regex
+                {
+                    var result = response.DeserializeResponse()["name"];
+                    Assert.That(result, Is.EqualTo("Raj1"), "Editing Done!");
+                }
+                else
+                {
+                    Console.WriteLine(response.Content);
+                    Assert.Fail("Error Request! - First Else");
+                }
             }
         }
     }
